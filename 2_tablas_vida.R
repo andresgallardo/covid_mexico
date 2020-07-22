dcb77dd45a05db172ef29f8e6dcdd5d4c12a05e9
andresgallardo <45669672+andresgallardo@users.noreply.github.com>
2020-07-22 00:50:51 -0500

    Add files via upload

---
+++ 2_tablas_vida.R
@@ -0,0 +1,197 @@
+### Script para hacer las tablas de vida
+#library(LifeTable)
+#library(dplyr)
+#library(ggplot2)
+
+#CARGAR DATAFRAME covid_mort_g DEL SCRIPT covid_mexico
+  t<-covid_mort_g%>%select(.,c(SEXO, CVE_GEO,DEFUNCIONES,POBLACION,MUERTES,DEF_COV,E))
+    names(t)<-c("sexo", "entidad","muertes","poblacion", "covid", "muertes_totales", "edad")
+    t<-arrange(t,entidad)
+    t$entidad_o<-t$entidad #crear variable para que no rompa con entidad 0
+    t$entidad<-t$entidad+1
+
+#CARGAR DATOS DEL IHME: https://covid19.healthdata.org/mexico
+  ihm<-read.csv("covid_mexico/ihme.csv")
+  ihme<-ihm%>%select(.,c(CVE_GEO,totdea_mean,totdea_lower,totdea_upper))
+    names(ihme)<-c("entidad_o","muertes_nov", "muertes_inf", "muertes_sup")
+    names(ihm)<-c("nombres","entidad")
+      nombres<-select(ihm,c(nombres,entidad))
+    ihme <-round(ihme[1:4],0)
+    ihme$edad=0
+    ihme$entidad=ihme$entidad+1
+
+#AGREGAR PROPORCIONES A t PORQUE IHME NO TIENE DESCOMPOSICIONES POR EDAD/SEXO
+  t<-t%>%group_by(entidad)%>%mutate(tot=sum(covid))
+      t<-merge(ihme,t,all=TRUE)
+      cols <- c("muertes_nov","muertes_sup", "muertes_inf")
+      t<- t%>% group_by(entidad) %>% mutate_at(cols, na.aggregate)
+
+#TABLAS DE VIDA
+  #crear tablas por sexo
+  tf<-filter(t,sexo==1) #mujeres (f)
+  tm<-filter(t,sexo==2) #hombres (m)
+
+  #preparar el loop para hacer la lista por estado
+  estados <- unique(t$entidad)
+
+#####ESCENARIO BASE: conapo: muertes
+  #crear vectores vacios
+  expf<-vector("list",length(estados))
+  expm<-vector("list",length(estados))
+
+  #hacer loop por estado para cada sexo
+  for (i in estados) {
+    tf_i<-filter(tf,entidad==i)
+    lt5f_i<-LT(tf_i$poblacion,tf_i$muertes, ages=c(0,1,seq(5,100,by=5)),sex = "female", axmethod = "preston", mxsmooth =FALSE, axsmooth = TRUE)
+    expf[[i]]<-lt5f_i$ex
+  }
+  for (i in estados) {
+    tm_i<-filter(tm,entidad==i)
+    lt5m_i<-LT(tm_i$poblacion,tm_i$muertes, ages=c(0,1,seq(5,100,by=5)),sex = "male", axmethod = "preston", mxsmooth = FALSE, axsmooth = TRUE)
+    expm[[i]]<-lt5m_i$ex
+  }
+  #agregar numero de entidad a cada vector
+  names(expf) <-estados
+  names(expm) <-estados
+  #convertir listas a dataframe
+  #mujeres
+  exf<-as.data.frame(expf)
+  exf<-exf[1,] #seleccionar e0
+  exf<- gather(exf,X)
+  exf<-exf%>%select(.,-c(X))
+  names(exf)<-("e0_f_proyecciones")
+  #hombres
+  exm<-as.data.frame(expm)
+  exm<-exm[1,]
+  exm<- gather(exm,X)
+  exm<-exm%>%select(.,-c(X))
+  names(exm)<-("e0_m_proyecciones")
+  #hacer dataframe final
+  e0_proyecciones<-cbind(exf,exm)
+
+#####ESCENARIO BAJO: covid secretaria de salud: muertes_totales
+  #crear vectores vacios
+  excf<-vector("list",length(estados))
+  excm<-vector("list",length(estados))
+
+  #hacer loop por estado para cada sexo
+  for (i in estados) {
+    tf_i<-filter(tf,entidad==i)
+    lt5f_i<-LT(tf_i$poblacion,tf_i$muertes_totales, ages=c(0,1,seq(5,100,by=5)),sex = "female", axmethod = "preston", mxsmooth = FALSE, axsmooth = TRUE)
+    excf[[i]]<-lt5f_i$ex
+  }
+  for (i in estados) {
+    tm_i<-filter(tm,entidad==i)
+    lt5m_i<-LT(tm_i$poblacion,tm_i$muertes_totales, ages=c(0,1,seq(5,100,by=5)),sex = "male", axmethod = "preston", mxsmooth = FALSE, axsmooth = TRUE)
+    excm[[i]]<-lt5m_i$ex
+  }
+  #agregar numero de entidad a cada vector
+  names(excf) <-estados
+  names(excm) <-estados
+  #convertir listas a dataframe
+  #mujeres
+  exf<-as.data.frame(excf)
+  exf<-exf[1,] #seleccionar e0
+  exf<- gather(exf,X)
+  exf<-exf%>%select(.,-c(X))
+  names(exf)<-("e0_f_covid")
+  #hombres
+  exm<-as.data.frame(excm)
+  exm<-exm[1,]
+  exm<- gather(exm,X)
+  exm<-exm%>%select(.,-c(X))
+  names(exm)<-("e0_m_covid")
+  #hacer dataframe final
+  e0_covid<-cbind(exf,exm)
+
+#ESCENARIO MEDIO: ihme medio con la misma proporcion por edad: muertes + muertes_nov
+  #crear vectores vacios
+  exih2f<-vector("list",length(estados))
+  exih2m<-vector("list",length(estados))
+
+  #hacer loop por estado para cada sexo
+  for (i in estados) {
+    tf_i<-filter(tf,entidad==i)
+    lt5f_i<-LT(tf_i$poblacion,tf_i$muertes+tf_i$muertes_nov*(tf_i$covid/tf_i$tot), ages=c(0,1,seq(5,100,by=5)),sex = "female", axmethod = "preston", mxsmooth = FALSE, axsmooth = TRUE)
+    exih2f[[i]]<-lt5f_i$ex
+  }
+  
+  for (i in estados) {
+    tm_i<-filter(tm,entidad==i)
+    lt5m_i<-LT(tm_i$poblacion,tm_i$muertes+tm_i$muertes_nov*(tm_i$covid/tm_i$tot), ages=c(0,1,seq(5,100,by=5)),sex = "male", axmethod = "preston", mxsmooth = FALSE, axsmooth = TRUE)
+    exih2m[[i]]<-lt5m_i$ex
+  }
+  #agregar numero de entidad a cada vector
+  names(exih2f) <-estados
+  names(exih2m) <-estados
+  #convertir listas a dataframe
+  #mujeres
+  exf<-as.data.frame(exih2f)
+  exf<-exf[1,] #seleccionar e0
+  exf<- gather(exf,X)
+  exf<-exf%>%select(.,-c(X))
+  names(exf)<-("e0_f_ihme")
+  #hombres
+  exm<-as.data.frame(exih2m)
+  exm<-exm[1,]
+  exm<- gather(exm,X)
+  exm<-exm%>%select(.,-c(X))
+  names(exm)<-("e0_m_ihme")
+  #hacer dataframe final
+  e0_ihme<-cbind(exf,exm)
+
+#ESCENARIO ALTO: ihme alto con la misma proporcion por edad: muertes + muertes_sup
+  #crear vectores vacios
+  exihs2f<-vector("list",length(estados))
+  exihs2m<-vector("list",length(estados))
+
+  #hacer loop por estado para cada sexo
+  for (i in estados) {
+    tf_i<-filter(tf,entidad==i)
+    lt5f_i<-LT(tf_i$poblacion,tf_i$muertes+tf_i$muertes_sup*(tf_i$covid/tf_i$tot), ages=c(0,1,seq(5,100,by=5)),sex = "female", axmethod = "preston", mxsmooth = FALSE, axsmooth = TRUE)
+    exihs2f[[i]]<-lt5f_i$ex
+  }
+  for (i in estados) {
+    tm_i<-filter(tm,entidad==i)
+    lt5m_i<-LT(tm_i$poblacion,tm_i$muertes+tm_i$muertes_sup*(tm_i$covid/tm_i$tot), ages=c(0,1,seq(5,100,by=5)),sex = "male", axmethod = "preston", mxsmooth = FALSE, axsmooth = TRUE)
+    exihs2m[[i]]<-lt5m_i$ex
+  }
+  #agregar numero de entidad a cada vector
+  names(exihs2f) <-estados
+  names(exihs2m) <-estados
+  #convertir listas a dataframe
+  #mujeres
+  exf<-as.data.frame(exihs2f)
+  exf<-exf[1,] #seleccionar e0
+  exf<- gather(exf,X)
+  exf<-exf%>%select(.,-c(X))
+  names(exf)<-("e0_f_ihme_sup")
+  #hombres
+  exm<-as.data.frame(exihs2m)
+  exm<-exm[1,]
+  exm<- gather(exm,X)
+  exm<-exm%>%select(.,-c(X))
+  names(exm)<-("e0_m_ihme_sup")
+  #hacer dataframe final
+  e0_ihme_sup<-cbind(exf,exm)
+
+####COMBINAR DATAFRAMES
+  e0_mexico<-cbind(e0_proyecciones,e0_covid,e0_covid_doble,e0_ihme,e0_ihme_sup ) #hay q actualizar
+  e0_mexico$entidad<-estados-1
+  e0_mexico <-round(e0_mexico[1:11],2)
+    nombres_a<-read.csv("covid_mexico/nombres_a.csv")
+    nombres_ac<-unique(nombres_a$nombres_a)
+  e0_mexico<-merge(e0_mexico,nombres_a)
+  e0_mexico<-merge(e0_mexico,nombres)
+  #crear las diferencias
+  e0_mexico$dif_f_co<-round(e0_mexico$e0_f_covid-e0_mexico$e0_f_proyecciones,2)
+  e0_mexico$dif_m_co<-round(e0_mexico$e0_m_covid-e0_mexico$e0_m_proyecciones,2)
+  e0_mexico$dif_f_ih<-round(e0_mexico$e0_f_ihme-e0_mexico$e0_f_proyecciones,2)
+  e0_mexico$dif_m_ih<-round(e0_mexico$e0_m_ihme-e0_mexico$e0_m_proyecciones,2)
+  e0_mexico$dif_f_ihs<-round(e0_mexico$e0_f_ihme_sup-e0_mexico$e0_f_proyecciones,2)
+  e0_mexico$dif_m_ihs<-round(e0_mexico$e0_m_ihme_sup-e0_mexico$e0_m_proyecciones,2)
+  
+  diferencias<-e0_mexico%>%select(.,c(e0_f_proyecciones,e0_m_proyecciones,dif_f_co,dif_m_co,dif_f_ih,dif_m_ih,dif_f_ihs,dif_m_ihs,nombres))
+  write_xlsx(diferencias,"covid_mexico/diferencias.xlsx")
+
+
---
+++ 3_graficas.R
@@ -0,0 +1,113 @@
+##MUJERES
+svg("covid_mexico/f_escenarios.svg")
+ggplot(e0_mexico, aes(y = reorder(nombres,e0_f_proyecciones),
+                      x = e0_f_covid,
+                      xend = e0_f_proyecciones)) +
+  geom_dumbbell(aes(y = reorder(nombres,e0_f_proyecciones),
+                    x = e0_f_covid,
+                    xend = e0_f_ihme),
+                colour_x = "grey30",
+                colour_xend = "grey50",
+                size_x=1.5,
+                size_xend=1.5)+
+  geom_dumbbell(aes(y = reorder(nombres,e0_f_proyecciones),
+                    x = e0_f_ihme,
+                    xend = e0_f_ihme_sup),
+                colour_x = "grey50",
+                colour_xend = "grey70",
+                size_x=1.5,
+                size_xend=1.5)+
+  geom_dumbbell(aes(y = reorder(nombres,e0_f_proyecciones),
+                    x = e0_f_covid,
+                    xend = e0_f_proyecciones),
+                colour_x = "grey30",
+                colour_xend = "black",
+                size_x=1.5,
+                size_xend=1.5)+
+  scale_x_continuous(breaks=c(74,75,76,77,78,79))+
+  labs(title = "",
+       x = "Expectativa de Vida (años) \n (Diferencia en años respecto a CONAPO)",
+       y = "")+
+  theme_bw() +
+  theme(legend.position="right",
+        axis.text.y = element_text(size = 9),
+        panel.grid.major.x=element_blank(),
+        panel.grid.minor.x=element_blank(),
+        panel.border=element_blank(),
+        axis.line = element_line(size = .4, colour = "black"),
+        axis.title.x = element_text(size = 9)) +
+  scale_y_discrete(expand = expansion(mult = c(0.03, 0.03))) +
+  geom_text(color="black", size=2, vjust=1.7,
+            aes(x=e0_f_proyecciones, label=e0_f_proyecciones))+
+  geom_text(color="black", size=2, vjust=1.7,
+           aes(x=e0_f_covid, label=dif_f_co))+
+  geom_text(aes(x=e0_f_ihme, label=dif_f_ih),
+            color="black", size=2, vjust=1.7) +
+  geom_text(aes(x=e0_f_ihme_sup, label=dif_f_ihs),
+              color="black", size=2, vjust=1.7) +
+  geom_text(aes(y="Ciudad de Mexico",x=79.42, label="CONAPO"),
+            color="black", size=2, vjust=-1.05 , fontface="bold")+
+  geom_text(aes(y="Ciudad de Mexico",x=78.58, label="Bajo (SS)"),
+            color="grey30", size=2, vjust=-1.05, fontface="bold")+
+  geom_text(aes(y="Ciudad de Mexico",x=78.02, label="Medio (IHME)"),
+          color="grey50", size=2, vjust=-1.05, fontface="bold")+
+  geom_text(aes(y="Ciudad de Mexico",x=77.48, label="Alto (IHME)"),
+            color="grey70", size=2, vjust=-1.05, fontface="bold")
+dev.off()
+
+##HOMBRES
+svg("covid_mexico/m_escenarios.svg")
+ggplot(e0_mexico, aes(y = reorder(nombres,e0_m_proyecciones),
+                      x = e0_m_covid,
+                      xend = e0_m_proyecciones)) +
+  geom_dumbbell(aes(y = reorder(nombres,e0_m_proyecciones),
+                    x = e0_m_covid,
+                    xend = e0_m_ihme),
+                colour_x = "grey30",
+                colour_xend = "grey50",
+                size_x=1.5,
+                size_xend=1.5)+
+  geom_dumbbell(aes(y = reorder(nombres,e0_m_proyecciones),
+                    x = e0_m_ihme,
+                    xend = e0_m_ihme_sup),
+                colour_x = "grey50",
+                colour_xend = "grey70",
+                size_x=1.5,
+                size_xend=1.5)+
+  geom_dumbbell(aes(y = reorder(nombres,e0_m_proyecciones),
+                    x = e0_m_covid,
+                    xend = e0_m_proyecciones),
+                colour_x = "grey30",
+                colour_xend = "black",
+                size_x=1.5,
+                size_xend=1.5)+
+  scale_x_continuous(breaks=c(66,67,68,69,70,71,72,73))+
+  labs(title = "",
+       x = "Expectativa de Vida (años) \n (Diferencia en años respecto a CONAPO)",
+       y = "")+
+  theme_bw() +
+  theme(legend.position="right",
+        axis.text.y = element_text(size = 9),
+        panel.grid.major.x=element_blank(),
+        panel.grid.minor.x=element_blank(),
+        panel.border=element_blank(),
+        axis.line = element_line(size = .4, colour = "black"),
+        axis.title.x = element_text(size = 9)) +
+  scale_y_discrete(expand = expansion(mult = c(0.03, 0.03))) +
+  geom_text(color="black", size=2, vjust=1.7,
+            aes(x=e0_m_proyecciones, label=e0_m_proyecciones))+
+  geom_text(color="black", size=2, vjust=1.7,
+            aes(x=e0_m_covid, label=dif_m_co))+
+  geom_text(aes(x=e0_m_ihme, label=dif_m_ih),
+            color="black", size=2, vjust=1.7) +
+  geom_text(aes(x=e0_m_ihme_sup, label=dif_m_ihs),
+            color="black", size=2, vjust=1.7) +
+  geom_text(aes(y="Ciudad de Mexico",x=73.50, label="CONAPO"),
+            color="black", size=2, vjust=-1.05 , fontface="bold")+
+  geom_text(aes(y="Ciudad de Mexico",x=71.79, label="Bajo (SS)"),
+            color="grey30", size=2, vjust=-1.05, fontface="bold")+
+  geom_text(aes(y="Ciudad de Mexico",x=70.68, label="Medio (IHME)"),
+            color="grey50", size=2, vjust=-1.05, fontface="bold")+
+  geom_text(aes(y="Ciudad de Mexico",x=69.64, label="Alto (IHME)"),
+            color="grey70", size=2, vjust=-1.05, fontface="bold")
+dev.off()
